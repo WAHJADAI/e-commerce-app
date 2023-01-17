2841a16e45ea2a879550ad0cbd2a4b8d11f4a2db
unknown <drafzaza123@icloud.com>
2023-01-17 23:08:03 +0700

    create page home

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './style/App.css'
+import {Route,Routes} from "react-router-dom"
+
+
+function HomePage(){
+  return(
+    <div>HomePage</div>
+  )
+}
+
+function AboutPage(){
+  return(
+    <div>AboutPage</div>
+  )
+}
+
+function ErrorPage(){
+  return <div><h2><i>Error 404 not found</i></h2></div>
+}
 
 function App() {
 
 
   return (
-    <div className="App">
-      hello world
+    <div >
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="about" element={<AboutPage/>}/>
+        <Route path='*' element={<ErrorPage/>}/>
+      </Routes>
     </div>
   )
 }
