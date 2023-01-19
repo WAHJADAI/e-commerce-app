926acfb29f033affc523383e01c6251c98be4a9a
unknown <drafzaza123@icloud.com>
2023-01-19 10:58:37 +0700

    create input Sign In form

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -3,7 +3,13 @@ import React from "react";
 function SignIn(){
     return(
         <div>
-            sign in
+            <form>
+                <h2>Sign In</h2>
+                <label htmlFor="email">email</label>
+                <input type="email" placeholder="email" id="email" name="email"/>
+                <label htmlFor="password">password</label>
+                <input type="password" placeholder="***********" id="password" name="password"/>
+            </form>
         </div>
     )
 }
