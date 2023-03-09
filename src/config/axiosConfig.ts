12abe4b55a59be5250a24ae88234b11b2a79abac
unknown <drafzaza123@icloud.com>
2023-03-09 15:04:53 +0700

    Add axios request and response

--- src/config/axiosConfig.ts
+++ src/config/axiosConfig.ts
@@ -1,8 +1,25 @@
 import axios from "axios";
 
-const clientApi = axios.create({
+const axiosApiInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
+axiosApiInstance.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
 
+axiosApiInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+const clientApi = axiosApiInstance;
 export default clientApi;
