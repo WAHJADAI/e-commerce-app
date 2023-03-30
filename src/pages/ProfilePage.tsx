b445e9a7090503a4dd676470e8525b1ad22940e9
unknown <drafzaza123@icloud.com>
2023-03-30 15:52:10 +0700

    add Personal Information form and Change Password form

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 const ProfileContent = styled.div`
   display: flex;
 
-  justify-content: center;
+  justify-content: start;
   justify-items: center;
 `;
 const PhotoProfile = styled.div`
@@ -16,21 +16,68 @@ const PhotoProfile = styled.div`
 
   background-color: blue;
 `;
+
+const CardForm = styled.form`
+  display: flex;
+  justify-content: center;
+`;
+const CardName = styled.div`
+  background-color: #8599b6;
+`;
+const CardAction = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: #b8bfc6;
+`;
 function ProfilePage() {
   return (
     <ProfileContent>
-      <div>Profile Page</div>
-      <form style={{ display: "flex", justifyContent: "center", flexDirection: "column" }}>
-        <PhotoProfile>Photo</PhotoProfile>
-        <input type='file' name='photo' id='' />
-        <span>E-mail</span>
-        <input type='email' name='' placeholder='Email' />
-        <span>Password</span>
-        <input type='password' name='' placeholder='password' />
-        <span>Conform Password</span>
-        <input type='password' name='' placeholder='Conform password' />
-        <button type='submit'>Submit</button>
-      </form>
+      <div
+        style={{
+          backgroundColor: "#7E81FF",
+          borderRadius: "10px",
+          padding: "10px",
+          margin: "10px",
+          position: "sticky",
+          height: "300px",
+        }}
+      >
+        <div>Account Management</div>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          width: "500px",
+          flexDirection: "column",
+        }}
+      >
+        <CardForm>
+          <CardName>Personal Information</CardName>
+          <CardAction>
+            <PhotoProfile>Photo</PhotoProfile>
+            <input type='file' name='photo' id='' />
+            <span>E-mail</span>
+            <input type='email' name='' placeholder='Email' />
+            <span>Name</span>
+            <input type='text' name='' placeholder='Name' />
+            <span>Surname</span>
+            <input type='text' name='' placeholder='Surname' />
+
+            <button type='submit'>Save </button>
+          </CardAction>
+        </CardForm>
+        <CardForm>
+          <CardName>Change Password</CardName>
+          <CardAction>
+            <span>Password</span>
+            <input type='password' name='' placeholder='password' />
+            <span>Conform Password</span>
+            <input type='password' name='' placeholder='Conform password' />
+            <button type='submit'>Save </button>
+          </CardAction>
+        </CardForm>
+      </div>
     </ProfileContent>
   );
 }
